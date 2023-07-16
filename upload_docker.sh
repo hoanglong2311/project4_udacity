415b2d173ce28ce9e59a18205de32afec7abc482
Ubuntu <ubuntu@ip-172-31-74-26.ec2.internal>
2023-07-16 18:45:36 +0000

    update

--- upload_docker.sh
+++ upload_docker.sh
@@ -10,20 +10,26 @@ docker_path=de150191
 
 # Step 2:  
 # Authenticate & tag
-# echo "Docker ID and Image: $docker_path"
 docker login
 
 # Check if the image is already tagged and remove it if necessary
+# Check if the image with the specified reference is already tagged
 image_tagged=$(docker image list --filter=reference="$docker_path/project-ml" | grep 'project-ml' | xargs)
+
+# If the image is already tagged, remove the tagged image
 if [[ -n $image_tagged ]]; then
   echo "Image already tagged, remove the tagged image."
 
+  # Extract the image name from the tagged image information
   name=$(echo "$image_tagged" | cut -f 1 -d " ")
 
+  # Extract the image tag from the tagged image information
   tag=$(echo "$image_tagged" | cut -f 2 -d " ")
 
+  # Remove the tagged image forcefully
   docker image remove --force "$name":"$tag"
 fi
+
 
 # Get information about the image by filters the lines containing "project-ml, only the lines representing Docker images with "project-ml" in their names.
 image_info=$(docker image list | grep 'project-ml' | xargs)
@@ -40,5 +46,4 @@ docker image list --filter=reference="$docker_path/project-ml"
 
 # Step 3:
 # Push image to a docker repository
-# Push the tagged image to the Docker repository
 docker image push "$docker_path/project-ml:$image_tag"
