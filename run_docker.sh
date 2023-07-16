ca325f7002771518cef236d9759a7fa61df07763
Hoang Long <70263014+hoanglong2311@users.noreply.github.com>
2023-07-17 05:56:30 +0700

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -4,12 +4,15 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+# Tag version for building with v1.0.0 to push it in dockerhub
 docker build --tag project-ml:v1.0.0 .
 
 # Step 2: 
 # List docker images
+# List out to all docker images in enviroment
 docker image list
 
 # Step 3:
 # Run flask app
+# Run docker images project-ml:v1.0.0 and expose to port 8000 in host machine
 docker run --detach --publish 8000:80 project-ml:v1.0.0
